e473b1b549d5c7d70bae15b241d7a551b6d32e08
gnspanchal <122634034+gnspanchal@users.noreply.github.com>
2024-01-20 15:58:39 +0000

    Basic Variables Use

---
+++ .devcontainer/Basic/Demo_veriables.js
@@ -0,0 +1,14 @@
+const accountId=6379
+let accountEmail="ganeshpanchal@gmail.com"
+var accountPassword="12345"
+accountCity="Pune"
+
+//accountId=2
+
+
+accountEmail="panchal@gmail.com"
+accountPassword="523657"
+accountCity="Bangaluru"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
