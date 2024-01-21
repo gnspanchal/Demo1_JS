25e1064a7d645f8dae96e334ba1821c735207d10
gnspanchal <122634034+gnspanchal@users.noreply.github.com>
2024-01-21 15:29:38 +0000

    Stack Premitive Non Premitive

---
+++ .devcontainer/Basic/Stack.js
@@ -0,0 +1,22 @@
+let myName="Ganesh Panchal"
+
+let fullName=myName
+
+fullName="Ganesh Mahadev Panchal"
+
+console.log(fullName);
+console.log(myName);
+
+let myDetails={
+    name:"Ganesh",
+    dob:"24Sep1989"
+}
+
+let myDetails1=myDetails;
+
+myDetails1.name="Anil"
+console.log(myDetails);
+console.log(myDetails.name);
+console.log(myDetails.dob);
+console.log(myDetails1.name);
+
