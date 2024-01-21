b1ea98fd3d36ab30f071d348511e412122baa081
gnspanchal <122634034+gnspanchal@users.noreply.github.com>
2024-01-21 06:04:59 +0000

    Comparision

---
+++ .devcontainer/Basic/Demo_ConversionOperation.js
@@ -0,0 +1,56 @@
+let score=33;
+//let score="33abc";
+//let score=null;
+//let score=undefined;
+//let score=true;
+
+// console.log(typeof score);
+// console.log(score);
+
+let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let value=3
+let negValue=-value
+//console.log(negValue);
+
+// console.log(2+5);
+// console.log(5-3);
+// console.log(2*2);
+// console.log(4/2);
+// console.log(5%2);
+// console.log(2**3);
+
+let str1="Hello"
+let str2=" Ganesh"
+
+let str3=str1+str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1" + 2+2);
+// console.log(1+2+"2");
+
+//console.log(+true);
+//console.log(true+);   //is not valid
+
+//prefix postfix js
+let gameCounter=100;
+//gameCounter++;
+++gameCounter;
+//console.log(gameCounter);
+
+
+let x = 3;
+const y = x++;
+//console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+//console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
